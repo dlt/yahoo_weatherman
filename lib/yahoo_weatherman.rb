8ca8542b898c2da049e5f9dac8b04b42810abc85
daltojr <daltojr@gmail.com>
2010-03-15 02:16:34 -0300

    renamed the library files and changed .gemspec file to include specs

---
+++ lib/yahoo_weatherman.rb
@@ -0,0 +1,56 @@
+path = File.expand_path(File.dirname(__FILE__))
+$LOAD_PATH.unshift(path) unless $LOAD_PATH.include?(path)
+
+require 'open-uri'
+require 'nokogiri'
+require 'yaml'
+
+require 'yahoo_weatherman/image'
+require 'yahoo_weatherman/response'
+
+module Weatherman
+  
+  VERSION = '0.2'
+
+  URI = 'http://weather.yahooapis.com/forecastrss'
+
+  I18N_YAML_DIR = File.expand_path(File.dirname(__FILE__) + '/../i18n/')
+
+  # = Client
+  #
+  # The weatherman client. Where it all begins.
+  #
+  class Client
+    attr_reader :options
+
+    def initialize(options = {})
+      @options = options
+    end
+    
+    #
+    # Just pass in a +woeid+ and it will return a Weatherman::Response object:w
+    #
+    def lookup_by_woeid(woeid)
+      raw = get request_url(woeid)
+      Response.new(raw, options[:lang])
+    end
+
+    private
+      def request_url(woeid)
+        URI + query_string(woeid)
+      end
+
+      def query_string(woeid)
+        "?w=#{woeid}&u=#{degrees_units}"
+      end
+
+      def degrees_units
+        options[:degrees_units] || 'c'
+      end
+
+      def get(url)
+        open(url) { |stream| stream.read } 
+      end
+  end
+
+end
---
+++ lib/yahoo_weatherman/image.rb
@@ -0,0 +1,15 @@
+module Weatherman
+  # == Image
+  #
+  # A hash-like object with the weather image attributes (width, height, url, etc..)
+  #
+  class Image
+    def initialize(doc)
+      @image_root = doc
+    end
+
+    def [](attr)
+      @image_root.xpath(attr).first.content 
+    end
+  end
+end
