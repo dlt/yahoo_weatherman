bf8bb20b2dc5248d808e289fc13156637cfb9920
Darren Cauthon <darren@cauthon.com>
2012-10-10 10:44:22 -0500

    Refactor spec to make room.

--- spec/yahoo_weatherman/yahoo_weatherman_spec.rb
+++ spec/yahoo_weatherman/yahoo_weatherman_spec.rb
@@ -1,13 +1,15 @@
 require 'spec_helper'
 
 describe Weatherman::Client do
-  before do
-    @client = Weatherman::Client.new
-  end
+  describe "#lookup_by_woeid"
+    before do
+      @client = Weatherman::Client.new
+    end
 
-  it 'should lookup by woeid' do
-    response = @client.lookup_by_woeid 455821
-    response.should be_instance_of(Weatherman::Response)
+    it 'should lookup by woeid' do
+      response = @client.lookup_by_woeid 455821
+      response.should be_instance_of(Weatherman::Response)
+    end
   end
 
 end
