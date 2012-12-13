356ce66684ca80be50346de8de49dadadc7fed4d
Jeffrey Chupp <jeff@semanticart.com>
2012-12-13 08:48:23 -0500

    use non-commercial YQL for woeid lookups

--- lib/yahoo_weatherman/woeid_lookup.rb
+++ lib/yahoo_weatherman/woeid_lookup.rb
@@ -3,21 +3,17 @@ require 'cgi'
 module Weatherman
   class WoeidLookup
 
-    def initialize(app_id)
-      @app_id = app_id
-    end
-
     def get_woeid(location)
       raw = get query_string(location)
       Nokogiri::HTML(raw).at_xpath('.//woeid').content
     rescue
       nil
     end
-    
+
     private
 
       def query_string(location)
-        "http://where.yahooapis.com/v1/places.q('#{::CGI.escape(location)}')?appid=#{@app_id}"
+        "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20geo.placefinder%20where%20text%3D%22#{::CGI.escape(location)}%22%20and%20gflags%3D%22R%22"
       end
 
       def get(url)
--- spec/yahoo_weatherman/yahoo_weatherman_spec.rb
+++ spec/yahoo_weatherman/yahoo_weatherman_spec.rb
@@ -15,13 +15,12 @@ describe Weatherman::Client do
   describe "#lookup_by_location" do
     describe "4729347 example" do
       it "should lookup by location" do
-        app_id = 'test_id'
         location = '78923'
 
         xml_result = WoeidHelper.open_test_file 'woeid_result_that_returns_4729347'
-        WoeidHelper.register_this_woeid_lookup_result xml_result, app_id, location
+        WoeidHelper.register_this_woeid_lookup_result xml_result, location
 
-        @client = Weatherman::Client.new( { :app_id => app_id } )
+        @client = Weatherman::Client.new()
         response = @client.lookup_by_location location
 
         response.should be_instance_of(Weatherman::Response)
@@ -30,13 +29,12 @@ describe Weatherman::Client do
 
     describe "12786745 example" do
       it "should lookup by location" do
-        app_id = 'apple'
         location = 'orange'
 
         xml_result = WoeidHelper.open_test_file 'woeid_result_that_returns_12786745'
-        WoeidHelper.register_this_woeid_lookup_result xml_result, app_id, location
+        WoeidHelper.register_this_woeid_lookup_result xml_result, location
 
-        @client = Weatherman::Client.new( { :app_id => app_id } )
+        @client = Weatherman::Client.new()
         response = @client.lookup_by_location location
 
         response.should be_instance_of(Weatherman::Response)
